fc6b2ad1ab6e68203911ff9e68eecfa046bdb1b5
Mimoriam <mimoriam420@gmail.com>
2022-02-02 18:59:50 +0500

    Commit #13 - Add Slugify and create a slug in the model before the data is saved

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema({
     name: {
@@ -113,5 +114,11 @@ const BootcampSchema = new mongoose.Schema({
         default: Date.now
     },
 });
+
+// Create bootcamp slug before the data is entered into the DB:
+BootcampSchema.pre('save', function (next) {
+    this.slug = slugify(this.name, { lower: true });
+    next();
+})
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
