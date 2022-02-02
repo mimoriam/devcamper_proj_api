1571f279f55f53b288dec0834e05be94b942cea2
Mimoriam <mimoriam420@gmail.com>
2022-02-02 16:48:51 +0500

    Commit #4 - Dotenv install + Config file creation

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const createError = require('http-errors');
 const express = require('express');
+const dotenv = require('dotenv');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+
+// Load env vars:
+dotenv.config({path: './config/config.env'});
 
 const app = express();
 
